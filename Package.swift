f06ec42a4e625f6ab93a162d7ce30a03205ff13f
Golan Bar-Nov <golan@onestep.co>
2026-06-08 07:28:25 +0300

    chore: promote SDK to 2.0.5 (final)
    Same xcframework bytes as 2.0.5-rc6 (checksum c4ab8864…); only the release URL changes.
    Internal provenance: 2.0.5-rc6 = c4f8dde7f13fbdb4c83b8860aa2b39e1a8631b5b

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OneStepSDK",
-            url: "https://github.com/OneStepRND/onestep-sdk-ios/releases/download/2.0.5-rc6/OneStepSDK.xcframework.zip",
+            url: "https://github.com/OneStepRND/onestep-sdk-ios/releases/download/2.0.5/OneStepSDK.xcframework.zip",
             checksum: "c4ab88649618d09d0ee454931c2feef543a6d3297c118e80e218b0eb43a7db3e"
         ),
     ]
